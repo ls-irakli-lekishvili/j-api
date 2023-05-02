e2357963dff89a1a6534a5795690cbc6170b7745
Irakli Lekishvili <yesntface@gmail.com>
2023-05-02 22:09:59 +0400

    receive request from all origin

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ import { getAIResponse } from "./AI.mjs";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.post("/prompt", async (req, res) => {
